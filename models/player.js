997c4266ec08afc1b6bd69427034ae39f5b5bf13
Fabianzkp <fabianzkp@gmail.com>
2025-03-27 15:44:30 +0100

    wk4 OAuth file

--- models/player.js
+++ models/player.js
@@ -4,7 +4,6 @@ const playerSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   countryName: { type: String, required: true },
-  gender: { type: String, required: true },
   age: { type: Number, required: true },
   position: { type: String, required: true },
   goals: { type: Number, required: true },
